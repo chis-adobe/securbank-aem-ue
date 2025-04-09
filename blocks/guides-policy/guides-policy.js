43fe4112e828d727a30cb54598073d22d9a85ae5
Liviu Chis <chis@adobe.com>
2025-04-08 20:16:54 -0400

    Added guides policy component

---
+++ blocks/guides-policy/guides-policy.js
@@ -0,0 +1,44 @@
+export default async function decorate(block) {
+  const props = [...block.children];
+  const path = props[0]?.textContent.trim();
+  const cachebuster = Math.floor(Math.random() * 1000);
+
+  const main = document.createElement('div');
+  main.innerHTML = "Loading Policy";
+
+  const url = path ? `https://publish-p130746-e1298459.adobeaemcloud.com/graphql/execute.json/CF-from-Guides/guidesCfByPath;path=${path};variation=main?ts=${cachebuster}`
+                : `https://publish-p130746-e1298459.adobeaemcloud.com/graphql/execute.json/CF-from-Guides/guidesCF?ts=${cachebuster}`;
+
+  const resp = await fetch(url);
+  if (!resp.ok) {
+    return;
+  }
+
+  const respJson = await resp.json();
+  const policyJson = path ? [respJson.data.cfFromAemGuidesByPath.item] : respJson.data.cfFromAemGuidesList.items[0];
+
+  // Clear main div
+  main.innerHTML = "";
+  
+  const title = document.createElement('div');
+  const descriptionDiv = document.createElement('div');
+  const topics = document.createElement('div');
+
+  title.classList.add("header");
+  descriptionDiv.classList.add("description");
+  topics.classList.add("topics");
+
+  title.innerHTML = policyJson.title;
+  descriptionDiv.append(policyJson.shortdesc.html);
+  topics.append(policyJson.topicData.html);
+
+  main.append(title);
+  main.append(descriptionDiv);
+  main.append(topics);
+
+  main.classList.add("guides-policy-container");
+
+  block.closest('.guides-policy-wrapper').classList.add(path ? "single" : "list");
+  block.innerHTML = "";
+  block.append(main);
+}
