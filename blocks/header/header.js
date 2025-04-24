83ce5886b7691cdf5e76e8dff22796847d13717b
Liviu Chis <chis@adobe.com>
2025-04-23 22:02:41 -0700

    Center home images

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -114,7 +114,7 @@ export function decorateNavAuth() {
 export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta).pathname : '/nav';
+  const navPath = navMeta ? new URL(navMeta).pathname : '/en/nav';
   const fragment = await loadFragment(navPath);
 
   // decorate nav DOM
